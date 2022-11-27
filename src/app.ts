83dfacd1ef4e0cd8dcfe59eff8b518eeed21a9bc
Olivér <oliver.mrakovics@gmail.com>
2022-11-27 11:02:47 +0100

    added the client build and served it from the express app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import { getCard } from "./controllers/cards-controller";
 import express, { Application } from "express";
+import path from "path";
 const app: Application = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 app.get("/api/cards", getCard);
 
