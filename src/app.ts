3d2565550dc17cf3c9325b21dbff4870f690f84b
Olivér <oliver.mrakovics@gmail.com>
2023-01-14 09:53:48 +0100

    backend: remove the static file serving and add a redirect function

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import express, { Application, Response, NextFunction } from "express";
-import path from "path";
+import express, { Application } from "express";
 import cors from "cors";
 import { getCard } from "./controllers/cards-controller";
 import { getThemes } from "./controllers/themes-controller";
+import { redirect } from "./controllers/redirect";
 const app: Application = express();
 
 const PORT = process.env.PORT || 8080;
@@ -10,13 +10,6 @@ const PORT = process.env.PORT || 8080;
 app.get("/api/cards", getCard);
 app.get("/api/themes", cors(), getThemes);
 
-app.use(
-  "/",
-  express.static(path.join(__dirname, "..", "client", "build")),
-  (_, res: Response, next: NextFunction) => {
-    res.setHeader("Cache-Control", "no-store, no-cache");
-    next();
-  }
-);
+app.get("*", redirect);
 
 app.listen(PORT, () => console.log("The server is running!"));
---
+++ src/controllers/redirect.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+
+export const redirect = (_: Request, res: Response) => {
+  res.redirect("https://0l1v3rr.github.io/github-readme-tech-stack");
+};
