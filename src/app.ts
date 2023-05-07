c985ab17b949fed0de58ba1f64b8eff1aafe9ba1
Olivér <oliver.mrakovics@gmail.com>
2023-05-07 10:13:39 +0200

    api: add new themes endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
 import express, { Application } from "express";
 import cors from "cors";
 import { getCard } from "./controllers/cards-controller";
-import { getThemes } from "./controllers/themes-controller";
+import { getTheme, getThemes } from "./controllers/themes-controller";
 import { redirect } from "./controllers/redirect";
-const app: Application = express();
 
+const app: Application = express();
 const PORT = process.env.PORT || 8080;
 
 app.get("/api/cards", getCard);
 app.get("/api/themes", cors(), getThemes);
+app.get("/api/themes/:name", cors(), getTheme);
 
 app.get("*", redirect);
 
--- src/controllers/themes-controller.ts
+++ src/controllers/themes-controller.ts
@@ -4,3 +4,16 @@ import { Request, Response } from "express";
 export const getThemes = (_: Request, res: Response) => {
   res.send([...themes.keys()].sort());
 };
+
+export const getTheme = (req: Request, res: Response) => {
+  const themeName = req.params.name;
+
+  const theme = themes.get(themeName);
+
+  if (!theme) {
+    res.status(404).send({ message: "Theme with this name doesn't exist." });
+    return;
+  }
+
+  res.send(theme);
+};
